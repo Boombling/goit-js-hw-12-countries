68f392a79988dcb10363af2a2c8506006a194fc2
Dmytro Filius <mindertan@gmail.com>
2021-04-15 01:22:42 +0300

    adds shablon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './styles.css';
+// import countryService from './services/country.service';
+import countriesCard from './templates/countries-card.hbs'
+
+const searchRef = document.querySelector('.search');
+
+searchRef.addEventListener('input', listenInput)
+function listenInput(e) {
+    // console.log(e.currentTarget.value);
+}
+
+// countryService.fetchCountries()
+//     .then(response => console.log(response))
+//     .catch(error => {
+//     console.log(error);
+//     });
+//  const BASE_URL = 'https://restcountries.eu';
+fetch('https://restcountries.eu/rest/v2/name/ukrain')
+    .then(response => {
+        return response.json();
+    })
+    .then(country => {
+        console.log(country);
+        const countries = countriesCard(country);
+        console.log(countries);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
---
+++ src/services/country.service.js
@@ -0,0 +1,19 @@
+// const BASE_URL = 'https://restcountries.eu';
+
+
+// export default {
+//     fetchCountries() {
+//         return fetch(`${BASE_URL}/rest/v2/name/ukrain`)
+//             .then(response => {
+//                 if (!response.ok) {
+//                     throw response;
+//                 }
+
+//                 return response.json();
+//             });
+//         .then(country => {
+//         const countries = countriesCard(country);
+//         console.log(countries);
+//     })
+//     },
+// }
